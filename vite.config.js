64f935b8e5b32ac004494e171f28e55922913a4e
nameC98 <rapmastercalz@gmail.com>
2024-10-06 09:21:13 +0200

    first commit

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
       "/opn": {
         target: "http://api.tikiti.co.zw",
         changeOrigin: true,
-        secure: false,
+
         rewrite: (path) => path.replace(/^\/opn/, "/opn"),
       },
     },
