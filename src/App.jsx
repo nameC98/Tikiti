ef5f38118455a315b15c3aa9ef82ca6fdbd60673
nameC98 <rapmastercalz@gmail.com>
2024-10-06 08:07:44 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 // export default App;
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Home from "./pages/Home.jsx";
-import HomeLayout from "./pages/HomeLayout.jsx";
-import { loader as homeloader } from "./pages/Home.jsx";
-import { loader as geteventloader } from "./pages/GetEventID.jsx";
-import GetEventID from "./pages/GetEventID.jsx";
+import Home from "./pages/Home";
+import HomeLayout from "./pages/HomeLayout";
+import { loader as homeloader } from "./pages/Home";
+import { loader as geteventloader } from "./pages/GetEventID";
+import GetEventID from "./pages/GetEventID";
 
 function App() {
   const router = createBrowserRouter([
